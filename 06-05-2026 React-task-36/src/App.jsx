d4264956bb8a849314196eb74cf8d0b02f489fe5
VENKATRAMAMOORTHY <86295246+GVRaman17@users.noreply.github.com>
2026-05-07 18:22:38 +0530

    Add files via upload

---
+++ 06-05-2026 React-task-36/src/App.jsx
@@ -0,0 +1,24 @@
+import Layout from "./Layout";
+import Button from "./Button";
+import UserCard from "./UserCard";
+
+function App() {
+  const handleClick = () => {
+    alert("Button Clicked!");
+  };
+
+  return (
+    <Layout>
+      <h2>Welcome to React Components</h2>
+
+      <UserCard name="Venkat" age={21} />
+
+      <Button
+        label="Click Me"
+        onClick={handleClick}
+      />
+    </Layout>
+  );
+}
+
+export default App;
